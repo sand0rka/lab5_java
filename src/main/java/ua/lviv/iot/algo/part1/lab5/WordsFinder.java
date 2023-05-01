d75ae6896f8ff5e6e9057f7c57e521a94495938c
sand0rka <115060928+sand0rka@users.noreply.github.com>
2023-05-01 18:16:57 +0300

    uploading code without main method

--- src/main/java/ua/lviv/iot/algo/part1/lab5/WordsFinder.java
+++ src/main/java/ua/lviv/iot/algo/part1/lab5/WordsFinder.java
@@ -1,43 +1,27 @@
 package ua.lviv.iot.algo.part1.lab5;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class WordsFinder {
+    public static final String patternWordOnSecondLastPosition = "[^.!?]+[.!?]";
+    public static final Pattern compiledPattern = Pattern.compile(patternWordOnSecondLastPosition);
 
-    public static List<String> findWordAtSecondLastPosition(final String text) {
-        List<String> wordsAtSecondLastPosition = new ArrayList<>();
+    public List<String> findWordAtSecondLastPosition(final String text) {
+        List<String> wordsAtSecondLastPosition = new LinkedList<>();
 
-        String pattern = "[^.!?]+[.!?]";
-        Pattern compiledPattern = Pattern.compile(pattern);
         Matcher matcher = compiledPattern.matcher(text);
 
         while (matcher.find()) {
             String sentence = matcher.group();
-            String[] words = sentence.split("\s+");
+            String[] words = sentence.trim().split("\s+");
             if (words.length > 1) {
                 String secondLastWord = words[words.length - 2];
                 wordsAtSecondLastPosition.add(secondLastWord);
             }
         }
-
-
         return wordsAtSecondLastPosition;
-    }
-
-    public static void main(final String[] args) {
-        Scanner scanner = new Scanner(System.in, "UTF-8");
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
-
-        List<String> words = findWordAtSecondLastPosition(input);
-        System.out.println("Words at the second-to-last position: ");
-        for (var word : words) {
-            System.out.println(word);
-        }
-
     }
 }
--- src/test/java/ua/lviv/iot/algo/part1/lab5/WordsFinderTest.java
+++ src/test/java/ua/lviv/iot/algo/part1/lab5/WordsFinderTest.java
@@ -7,12 +7,12 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WordsFinderTest {
-
+    WordsFinder wordsFinder = new WordsFinder();
     @Test
     void testFindWordsAtSecondLastPosition() {
         String text = "This is the first sentence. Here is the second sentence. And the third one!";
         List<String> expectedWords = List.of("first", "second", "third");
-        List<String> actualWords = WordsFinder.findWordAtSecondLastPosition(text);
+        List<String> actualWords = wordsFinder.findWordAtSecondLastPosition(text);
         assertEquals(expectedWords, actualWords);
     }
 
@@ -20,14 +20,14 @@ public class WordsFinderTest {
     void testFindWordsAtSecondLastPosition_EmptyText() {
         String text = "";
         List<String> expectedWords = List.of();
-        List<String> actualWords = WordsFinder.findWordAtSecondLastPosition(text);
+        List<String> actualWords = wordsFinder.findWordAtSecondLastPosition(text);
         assertEquals(expectedWords, actualWords);
     }
 
     @Test
     void testFindWordsAtSecondLastPosition_singleWordSentences() {
         String text = "Hello.This.Is.A.Test.";
-        List<String> words = WordsFinder.findWordAtSecondLastPosition(text);
+        List<String> words = wordsFinder.findWordAtSecondLastPosition(text);
         assertEquals(0, words.size());
     }
 
